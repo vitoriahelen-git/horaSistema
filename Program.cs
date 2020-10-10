0e387006070ae9d52a4b93dfe08fd32e534315bf
Vitoria <vitoriahellen3004@gmail.com>
2020-10-10 20:05:34 -0300

    Exiba a data e a hora atual do sistema.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace horaSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data e Hora do sistema");
+            DateTime datahora = DateTime.Now;
+            Console.Write($"Nesse momento são exatamente: {datahora} ");
+
+            
+        }
+    }
+}
